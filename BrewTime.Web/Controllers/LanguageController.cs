77a9da2182e116d7f35c51301ecfed5bb98bd386
Angelica-Solis <masolissi@est.utn.ac.cr>
2026-07-27 20:10:15 -0600

    Implementacion de traductor

---
+++ BrewTime.Web/Controllers/LanguageController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Localization;
+
+namespace BrewTime.Web.Controllers
+{
+    public class LanguageController : Controller
+    {
+        [HttpPost]
+        public IActionResult SetLanguage(string culture, string returnUrl)
+        {
+            // Guarda el idioma en una cookie persistente por 1 año
+            Response.Cookies.Append(
+                CookieRequestCultureProvider.DefaultCookieName,
+                CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture)),
+                new CookieOptions { Expires = DateTimeOffset.UtcNow.AddYears(1) }
+            );
+
+            return LocalRedirect(returnUrl);
+        }
+    }
+}
